0d31dffae174c8c1b468b3568e5196606aec1c29
DollyP <seundolapo00@gmail.com>
2024-10-04 15:31:30 +0100

    added new files

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const fareResult = document.getElementById("fare");
 const travelResult = document.querySelector(".travel_result");
 const journeyContainer = document.querySelector(".journey_container");
 
+const errorMessage = document.querySelector(".error_message");
+
 // calculateFareBtn.addEventListener("click", (e) => {
 //   fareResult.textContent += " 3.90";
 // });
@@ -21,47 +23,53 @@ const calculateBtn = async () => {
   travelResult.classList.remove("hide");
   journeyContainer.style.border = "2px solid black";
 
-  try {
-    journeyContainer.innerHTML = "LOADING!!!";
-    const response = await fetch(
-      `https://api.tfl.gov.uk/Journey/JourneyResults/${departureInputValue}/to/${destinationInputValue}`
-    );
-    const responseBody = await response.json();
-    console.log(responseBody.journeys);
+  if (departureInputValue === "" || destinationInputValue === "") {
+    alert("Fill details completely");
+    travelResult.classList.add("hide");
+    journeyContainer.style.border = "none";
+  } else {
+    try {
+      journeyContainer.innerHTML = "LOADING!!!";
+      const response = await fetch(
+        `https://api.tfl.gov.uk/Journey/JourneyResults/${departureInputValue}/to/${destinationInputValue}`
+      );
+      const responseBody = await response.json();
+      console.log(responseBody.journeys);
 
-    journeyContainer.innerHTML = "";
+      journeyContainer.innerHTML = "";
 
-    for (const journey of responseBody.journeys) {
-      let journeys = `<div class="journey">
+      for (const journey of responseBody.journeys) {
+        let journeys = `<div class="journey">
         <li class="departure_arrival_time"></li>
         <li class="journey_duration">${journey.duration}</li>
         <li class="platform"></li>
       </div>`;
-      //   console.log(`Arrival Time: ${journey.arrivalDateTime}`);
+        //   console.log(`Arrival Time: ${journey.arrivalDateTime}`);
 
-      //   console.log(`Duration: ${journey.duration} minutes`);
-      //   console.log(`Start Time: ${journey.startDateTime}`);
-      for (const leg of journey.legs) {
-        journeys = `<div class="journey">
+        //   console.log(`Duration: ${journey.duration} minutes`);
+        //   console.log(`Start Time: ${journey.startDateTime}`);
+        for (const leg of journey.legs) {
+          journeys = `<div class="journey">
         <li class="departure_arrival_time">${leg.departureTime.slice(
           11,
           16
         )} - ${leg.arrivalTime.slice(11, 16)}</li>
         <li class="journey_duration">${journey.duration} mins</li>
         <li class="platform"></li> </div>`;
-        // console.log(leg.departurePoint.commonName);
-        // console.log(leg.arrivalPoint.commonName);
-        for (const stopPoint of leg.path.stopPoints) {
-          console.log(
-            `Stop Point: ${stopPoint.name} - Arrival Time: ${journey.arrivalDateTime} , Duration: ${journey.duration} minutes , Start Time: ${journey.startDateTime}`
-          );
+          // console.log(leg.departurePoint.commonName);
+          // console.log(leg.arrivalPoint.commonName);
+          for (const stopPoint of leg.path.stopPoints) {
+            console.log(
+              `Stop Point: ${stopPoint.name} - Arrival Time: ${journey.arrivalDateTime} , Duration: ${journey.duration} minutes , Start Time: ${journey.startDateTime}`
+            );
+          }
         }
-      }
 
-      journeyContainer.insertAdjacentHTML("beforeend", journeys);
+        journeyContainer.insertAdjacentHTML("beforeend", journeys);
+      }
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
   }
 
   //   fareResult.textContent += " 3.90";
